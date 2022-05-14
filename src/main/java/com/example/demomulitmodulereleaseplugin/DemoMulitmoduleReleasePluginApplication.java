c09a270f46a998ae2747568911194fce4c10ee68
asasas234 <605405375@qq.com>
2022-05-14 12:33:49 +0800

    删除

---
+++ src/main/java/com/example/demomulitmodulereleaseplugin/DemoMulitmoduleReleasePluginApplication.java
@@ -0,0 +1,13 @@
+package com.example.demomulitmodulereleaseplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMulitmoduleReleasePluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMulitmoduleReleasePluginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demomulitmodulereleaseplugin/DemoMulitmoduleReleasePluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demomulitmodulereleaseplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMulitmoduleReleasePluginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
